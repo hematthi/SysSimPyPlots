0624f583a9c682ed70aa8eec1cb76d531ce88057
Matthias Yang He <myh7@psu.edu>
2022-08-27 10:55:42 -0400

    Add autodoc imports to try and fix failed import of functions_general

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,6 +24,8 @@ extensions = [
 templates_path = ['_templates']
 exclude_patterns = []
 
+autodoc_mock_imports = ["numpy", "matplotlib", "scipy"]
+
 
 
 # -- Options for HTML output -------------------------------------------------
