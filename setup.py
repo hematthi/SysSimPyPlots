9780124d64772a409faef58e023a9d68888b5ae3
Matthias Yang He <myh7@psu.edu>
2022-09-13 22:29:26 -0400

    Update setup.py and MANIFEST.in to include package data

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     include_package_data=True,
-    package_data={"": ["../data/*"]},
+    package_data={"syssimpyplots/data": ["*.csv", "*.txt"]}, # doesn't work though unless we include them in a MANIFEST.in file
 )
