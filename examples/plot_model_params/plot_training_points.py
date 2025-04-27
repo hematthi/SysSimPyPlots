f9c4be60b58d0d273492dfd3c82189fe8c4d1014
Matthias Yang He <mhe@nd.edu>
2025-04-27 14:46:19 -0700

    Update script for plotting training points for new runs

--- examples/plot_model_params/plot_training_points.py
+++ examples/plot_model_params/plot_training_points.py
@@ -26,38 +26,40 @@ from syssimpyplots.plot_params import *
 ##### To load the files with the GP evaluated points:
 
 savefigures = False
+transformed_rates = False
 
 #run_directory = 'Hybrid_NR20_AMD_model1/Fit_all_KS/Params12/GP_files/'
 #loadfiles_directory = '/Users/hematthi/Documents/NotreDame_Postdoc/CRC/Files/SysSim/Model_Optimization/' + run_directory
-run_directory = 'Hybrid_NR20_AMD_model1/Fit_all_KS/Params8/GP_files/'
+run_directory = 'Hybrid_NR20_AMD_model1/Fit_some_KS/Params8_fix_highM/GP_files/' #'Fit_all_KS/Params8/'
 loadfiles_directory = '/Users/hematthi/Documents/NPP_ARC_Modernize_Kepler/Personal_research/SysSim/Model_Optimization/' + run_directory
 savefigures_directory = '/Users/hematthi/Documents/GradSchool/Research/SysSim/Figures/Model_Optimization/' + run_directory
 model_name = 'Hybrid_NR20_AMD_model1'
 
-active_params_symbols = [r'$M_{\rm break,1}$',
-                         #r'$\ln{(\lambda_c)}$',
-                         #r'$\ln{(\lambda_p)}$',
+active_params_symbols = [#r'$M_{\rm break,1}$',
+                         r'$\ln{(\lambda_c)}$',
+                         r'$\ln{(\lambda_p)}$',
+                         r'$\ln{(\alpha_{\rm ret})}$',
                          r'$\mu_M$',
                          r'$R_{p,\rm norm}$',
                          #r'$\alpha_P$',
                          r'$\gamma_0$',
-                         r'$\gamma_1$',
+                         #r'$\gamma_1$',
                          r'$\sigma_0$',
-                         r'$\sigma_1$',
+                         #r'$\sigma_1$',
                          r'$\sigma_M$',
                          #r'$\sigma_P$',
-                         #r'$\alpha_{\rm ret}$',
                          ] # this list of parameter symbols must match the order of parameters in 'active_params_names'!
 dims = len(active_params_symbols)
 
-active_params_transformed_symbols = copy.deepcopy(np.array(active_params_symbols, dtype=object))
-#i_transformed, j_transformed = 1, 2
-#active_params_transformed_symbols[i_transformed] = r'$\ln{(\lambda_c \lambda_p)}$'
-#active_params_transformed_symbols[j_transformed] = r'$\ln{(\lambda_p/\lambda_c)}$'
+if transformed_rates:
+    active_params_transformed_symbols = copy.deepcopy(np.array(active_params_symbols, dtype=object))
+    i_transformed, j_transformed = 0, 1 #1, 2
+    active_params_transformed_symbols[i_transformed] = r'$\ln{(\lambda_c \lambda_p)}$'
+    active_params_transformed_symbols[j_transformed] = r'$\ln{(\lambda_p/\lambda_c)}$'
 
 # To load the training points:
-data_optim = load_training_points(dims, file_name_path=loadfiles_directory, file_name='Active_params_distances_table_best100000_every10.txt')
-data_train = load_training_points(dims, file_name_path=loadfiles_directory, file_name='Active_params_recomputed_distances_table_best100000_every10.txt')
+data_optim = load_training_points(dims, file_name_path=loadfiles_directory, file_name='Active_params_distances_table_best10000_every1.txt')
+data_train = load_training_points(dims, file_name_path=loadfiles_directory, file_name='Active_params_recomputed_distances_table_best10000_every1.txt')
 active_params_names = np.array(data_train['active_params_names'])
 
 
@@ -79,6 +81,7 @@ if savefigures:
 plt.show()
 
 ##### If we want to compute and plot the un-logged rates (i.e. lambda_c, lambda_p):
+##### NOT UPDATED #####
 #data_train['xtrain'][:,[1,2]] = np.exp(data_train['xtrain'][:,[1,2]])
 #active_params_symbols[1], active_params_symbols[2] = r'$\lambda_c$', r'$\lambda_p$'
 #active_params_transformed_symbols[1], active_params_transformed_symbols[2] = r'$\lambda_c \lambda_p$', r'$\lambda_p /\lambda_c$'
@@ -86,7 +89,8 @@ plt.show()
 # To make corner plots for the GP training points:
 
 plot_cornerpy_wrapper(active_params_symbols, data_train['xtrain'], save_name=savefigures_directory + model_name + '_training_corner.pdf', save_fig=savefigures)
-#plot_cornerpy_wrapper(active_params_transformed_symbols, transform_sum_diff_params(data_train['xtrain'], i_transformed, j_transformed), save_name=savefigures_directory + model_name + '_training_transformed_corner.pdf', save_fig=savefigures)
+if transformed_rates:
+    plot_cornerpy_wrapper(active_params_transformed_symbols, transform_sum_diff_params(data_train['xtrain'], i_transformed, j_transformed), save_name=savefigures_directory + model_name + '_training_transformed_corner.pdf', save_fig=savefigures)
 plt.show()
 
 
@@ -101,6 +105,7 @@ xmin_table = load_table_points_min_GP(file_name, file_name_path=loadfiles_direct
 xmins = xmin_table[active_params_names].view((float, dims))
 
 plot_cornerpy_wrapper(active_params_symbols, data_train['xtrain'], xpoints_extra=xmins, save_name=savefigures_directory + model_name + '_training_corner.pdf', save_fig=savefigures)
-plot_cornerpy_wrapper(active_params_transformed_symbols, transform_sum_diff_params(data_train['xtrain'], 1, 2), xpoints_extra=transform_sum_diff_params(xmins, 1, 2), save_name=savefigures_directory + model_name + '_training_transformed_corner.pdf', save_fig=savefigures)
+if transformed_rates:
+    plot_cornerpy_wrapper(active_params_transformed_symbols, transform_sum_diff_params(data_train['xtrain'], i_transformed, j_transformed), xpoints_extra=transform_sum_diff_params(xmins, i_transformed, j_transformed), save_name=savefigures_directory + model_name + '_training_transformed_corner.pdf', save_fig=savefigures)
 plt.close()
 '''
